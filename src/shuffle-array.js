2219289a8afc8e572923c0825ddc27e96991dfca
dedurus <dedurac@gmail.com>
2016-04-07 02:28:57 +0200

    shuffle-array.js added

---
+++ src/shuffle-array.js
@@ -0,0 +1,26 @@
+// Shuffle an array (Fisher–Yates shuffle https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle)
+
+function shuffle(array) {
+    var currentIndex = array.length
+      , temporaryValue
+      , randomIndex
+      ;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+// test
+shuffle(['s','h','u','f','l', 'f', 'e']);
