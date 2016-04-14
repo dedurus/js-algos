e60c32c7a6f7f1b17abf1c420537a6d1562a6614
dedurus <dedurac@gmail.com>
2016-04-14 22:02:30 +0200

    2 more versions added

--- src/nth-fibonacci-number.js
+++ src/nth-fibonacci-number.js
@@ -24,3 +24,42 @@ function fibonacci_recursive(n){
 }
 
 fibonacci_recursive(12);
+
+
+// 3. Dynamic
+function dynFib(n) {
+  var val = [];
+  for (var i = 0; i <= n; ++i) {
+    val[i] = 0;
+  }
+  if (n == 1 || n == 2) {
+    return 1;
+  }
+  else {
+    val[1] = 1;
+    val[2] = 2;
+    for (var i = 3; i <= n; ++i) {
+      val[i] = val[i-1] + val[i-2];
+    }
+  return val[n-1];
+  }
+}
+// test
+console.log(dynFib(12));
+
+
+// 4. Iterative (same as dynamic #3, but without using an array)
+function iterFib(n) {
+  var last = 1;
+  var nextLast = 1;
+  var result = 1;
+  for (var i = 2; i < n; ++i) {
+    result = last + nextLast;
+    nextLast = last;
+    last = result;
+  }
+  return result;
+}
+
+// test
+console.log(iterFib(12));
